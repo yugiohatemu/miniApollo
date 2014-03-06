5545f15cadf262db3a3e4fbbc4727f5040847565
yugiohatemu <yue4huang@gmail.com>
2014-03-06 10:44:24 -0500

    Modify DataPool into PriorityPeer to accomodate

---
+++ Decision/priorityPeer.cpp
@@ -0,0 +1,33 @@
+//
+//  dataPool.cpp
+//  Decision
+//
+//  Created by Yue Huang on 2014-02-28.
+//  Copyright (c) 2014 Yue Huang. All rights reserved.
+//
+
+#include "priorityPeer.h"
+#include <stdlib.h>
+#include <iostream>
+#include "log.h"
+
+PriorityPeer::PriorityPeer(){
+    ts_list.reserve(50);
+}
+
+PriorityPeer::~PriorityPeer(){
+    for (unsigned int i = 0; i < bb_list.size(); i++) {
+        delete bb_list[i];
+    }
+}
+void PriorityPeer::add_ts(uint64_t ts){
+    boost::mutex::scoped_lock scoped_lock(mutex);
+    ts_list.push_back(ts);
+}
+
+//ask for BB copy
+void PriorityPeer::add_bb(BackBundle * bb){
+    boost::mutex::scoped_lock scoped_lock(mutex);
+    //make a copy of BB
+    bb_list.push_back(new BackBundle(*bb));
+}
---
+++ Decision/priorityPeer.h
@@ -0,0 +1,32 @@
+//
+//  dataPool.h
+//  Decision
+//
+//  Created by Yue Huang on 2014-02-28.
+//  Copyright (c) 2014 Yue Huang. All rights reserved.
+//
+
+#ifndef __Decision__dataPool__
+#define __Decision__dataPool__
+
+#include <vector>
+#include <boost/thread/mutex.hpp>
+#include <inttypes.h>
+#include "backBundle.h"
+
+//Acts as a Priority peer so that it is always on line to keep all data
+
+class PriorityPeer{
+    std::vector<uint64_t> ts_list;
+    std::vector<BackBundle *> bb_list;
+    boost::mutex mutex;
+public:
+    PriorityPeer();
+    ~PriorityPeer();
+    void add_ts(uint64_t ts);
+    void add_bb(BackBundle * bb);
+    //if no body is online
+    //ask for update from PriorityPeer
+};
+
+#endif /* defined(__Decision__dataPool__) */
